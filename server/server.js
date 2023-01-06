3701127154a17644c3eed35dbdda2af429412b79
miscarriage87 <96748832+miscarriage87@users.noreply.github.com>
2023-01-06 02:41:11 +0100

    t

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@
  console.log(process.env.OPENAI_API_KEY);
 
  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+   apiKey: process.env.OPENAI_API_KEY,
  });
 
  const openai = new OpenAIApi(configuration);
